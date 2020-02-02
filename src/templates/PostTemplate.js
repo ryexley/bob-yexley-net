a743d5fb2ff5af28c6ca3f5b7223e045c2bca335
Bob Yexley <bob@yexley.net>
2020-02-02 13:07:38 -0500

    fix: fixed code snippet rendering

--- src/templates/PostTemplate.js
+++ src/templates/PostTemplate.js
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types"
 import React from "react"
 import {graphql} from "gatsby"
-require("prismjs/themes/prism.css")
+require("prismjs/themes/prism-coy.css")
 
 import Seo from "../components/Seo"
 import Article from "../components/Article"
