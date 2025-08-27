6adeed79122d8dc3d81a6ec7bdf71b5bd7c761eb
Bob Yexley <bob@yexley.net>
2025-08-27 09:45:37 -0400

    fix: updated/fixed main site favicon

--- src/layouts/shared.tsx
+++ src/layouts/shared.tsx
@@ -36,7 +36,7 @@ export const links = [
   },
   {
     rel: "icon",
-    href: "/images/harvest-archery-logo.svg",
+    href: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>👽</text></svg>",
     type: "image/svg+xml",
   },
 ]
